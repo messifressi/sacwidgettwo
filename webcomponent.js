ccafbd65347bb2b44e17957123b7a3328c2537cf
messifressi <67953592+messifressi@users.noreply.github.com>
2020-11-27 17:15:09 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -615,7 +615,7 @@
 			console.log(newValues);
 			var ks01 = this._shadowRoot.querySelector("#KS01");
 			console.log(ks01);
-			ks01.setAttribute("fill", getColor(newValues[1]));
+			//ks01.setAttribute("fill", getColor(newValues[1]));
 			/*
 			var ks02 = this._shadowRoot.querySelector("#KS02");
 			var ks03 = this._shadowRoot.querySelector("#KS03");
